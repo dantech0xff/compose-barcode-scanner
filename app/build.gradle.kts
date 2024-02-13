4d94783738381e59787646fa22aad1ad2ebed24c
Dan Tech <danhtran.dev@outlook.com>
2024-02-13 22:03:27 +0700

    update accompanist

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.creative.qrcodescanner"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0-alpha-01"
+        versionCode = 2
+        versionName = "1.0.0-alpha-02"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
